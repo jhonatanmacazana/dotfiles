7399aa8693be83610a8d3d2a2541b09908b2acad
Jhonatan M <46355455+jhonatanmacazana@users.noreply.github.com>
2020-11-15 19:21:12 -0500

    added shell aliases, exports and functions

---
+++ .config/shell/functions.sh
@@ -0,0 +1,201 @@
+#!/usr/bin/env bash
+
+# Create a new directory and enter it
+function mkd() {
+    mkdir -p "$@" && cd "$_"
+}
+
+# Change working directory to the top-most Finder window location
+function cdf() { # short for `cdfinder`
+    cd "$(osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)')"
+}
+
+# Create a .tar.gz archive, using `zopfli`, `pigz` or `gzip` for compression
+function targz() {
+    local tmpFile="${@%/}.tar"
+    tar -cvf "${tmpFile}" --exclude=".DS_Store" "${@}" || return 1
+
+    size=$(
+        stat -f"%z" "${tmpFile}" 2>/dev/null # macOS `stat`
+        stat -c"%s" "${tmpFile}" 2>/dev/null # GNU `stat`
+    )
+
+    local cmd=""
+    if ((size < 52428800)) && hash zopfli 2>/dev/null; then
+        # the .tar file is smaller than 50 MB and Zopfli is available; use it
+        cmd="zopfli"
+    else
+        if hash pigz 2>/dev/null; then
+            cmd="pigz"
+        else
+            cmd="gzip"
+        fi
+    fi
+
+    echo "Compressing .tar ($((size / 1000)) kB) using \`${cmd}\`…"
+    "${cmd}" -v "${tmpFile}" || return 1
+    [ -f "${tmpFile}" ] && rm "${tmpFile}"
+
+    zippedSize=$(
+        stat -f"%z" "${tmpFile}.gz" 2>/dev/null # macOS `stat`
+        stat -c"%s" "${tmpFile}.gz" 2>/dev/null # GNU `stat`
+    )
+
+    echo "${tmpFile}.gz ($((zippedSize / 1000)) kB) created successfully."
+}
+
+# Determine size of a file or total size of a directory
+function fs() {
+    if du -b /dev/null >/dev/null 2>&1; then
+        local arg=-sbh
+    else
+        local arg=-sh
+    fi
+    if [[ -n "$@" ]]; then
+        du $arg -- "$@"
+    else
+        du $arg .[^.]* ./*
+    fi
+}
+
+# Use Git’s colored diff when available
+hash git &>/dev/null
+if [ $? -eq 0 ]; then
+    function diff() {
+        git diff --no-index --color-words "$@"
+    }
+fi
+
+# Create a data URL from a file
+function dataurl() {
+    local mimeType=$(file -b --mime-type "$1")
+    if [[ $mimeType == text/* ]]; then
+        mimeType="${mimeType};charset=utf-8"
+    fi
+    echo "data:${mimeType};base64,$(openssl base64 -in "$1" | tr -d '\n')"
+}
+
+# Start an HTTP server from a directory, optionally specifying the port
+function server() {
+    local port="${1:-8000}"
+    sleep 1 && open "http://localhost:${port}/" &
+    # Set the default Content-Type to `text/plain` instead of `application/octet-stream`
+    # And serve everything as UTF-8 (although not technically correct, this doesn’t break anything for binary files)
+    python -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] = "text/plain";\nfor key, value in map.items():\n\tmap[key] = value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$port"
+}
+
+# Start a PHP server from a directory, optionally specifying the port
+# (Requires PHP 5.4.0+.)
+function phpserver() {
+    local port="${1:-4000}"
+    local ip=$(ipconfig getifaddr en1)
+    sleep 1 && open "http://${ip}:${port}/" &
+    php -S "${ip}:${port}"
+}
+
+# Compare original and gzipped file size
+function gz() {
+    local origsize=$(wc -c <"$1")
+    local gzipsize=$(gzip -c "$1" | wc -c)
+    local ratio=$(echo "$gzipsize * 100 / $origsize" | bc -l)
+    printf "orig: %d bytes\n" "$origsize"
+    printf "gzip: %d bytes (%2.2f%%)\n" "$gzipsize" "$ratio"
+}
+
+# Run `dig` and display the most useful info
+function digga() {
+    dig +nocmd "$1" any +multiline +noall +answer
+}
+
+# Normalize `open` across Linux, macOS, and Windows.
+# This is needed to make the `o` function (see below) cross-platform.
+if [ ! $(uname -s) = 'Darwin' ]; then
+    if grep -q microsoft /proc/version; then
+        # Ubuntu on Windows using the Linux subsystem
+        alias open='explorer.exe'
+    else
+        alias open='xdg-open'
+    fi
+fi
+
+# `o` with no arguments opens the current directory, otherwise opens the given
+# location
+function o() {
+    if [ $# -eq 0 ]; then
+        open .
+    else
+        open "$@"
+    fi
+}
+
+# `tre` is a shorthand for `tree` with hidden files and color enabled, ignoring
+# the `.git` directory, listing directories first. The output gets piped into
+# `less` with options to preserve color and line numbers, unless the output is
+# small enough for one screen.
+function tre() {
+    tree -aC -I '.git|node_modules|bower_components' --dirsfirst "$@" | less -FRNX
+}
+
+#--------------------------------------
+# Functions
+#--------------------------------------
+
+mkcd() {
+    mkdir -p "$*"
+    cd "$*"
+}
+
+cdl() {
+    cd "$*"
+    ls -la
+}
+
+#--------------------------------------
+# -> General compilators
+f_gccrun() {
+    gcc -o /tmp/temp.out "$1" && /tmp/temp.out && rm /tmp/temp.out
+}
+
+f_g++run() {
+    g++ -o /tmp/temp.out "$1" && /tmp/temp.out && rm /tmp/temp.out
+}
+
+#--------------------------------------
+# -> OMP
+f_omprun() {
+    g++ -o /tmp/temp.out -D_OMP -fopenmp "$1" && /tmp/temp.out && rm /tmp/temp.out
+}
+
+f_ompcomp() {
+    g++ -o temp.out -D_OMP -fopenmp "$1"
+}
+
+#--------------------------------------
+# -> MPI
+f_mpirun() {
+    mpic++ -o /tmp/temp.out -g "$1" && mpirun -np "$2" /tmp/temp.out && rm /tmp/temp.out
+}
+
+f_mpicomp() {
+    mpic++ -o temp.out -g "$1"
+}
+
+#--------------------------------------
+# -> MPI + OMP
+f_mpi_omp_run() {
+    mpic++ -o /tmp/temp.out -g "$1" -fopenmp && mpirun -np "$2" /tmp/temp.out && rm /tmp/temp.out
+}
+
+f_mpi_omp_comp() {
+    mpic++ -o /tmp/temp.out -g "$1" -fopenmp
+}
+
+#--------------------------------------
+# -> Cluster
+send() {
+    scp -r "$*" cluster:~/submits
+}
+
+sendToDir() {
+    scp -r "$1" cluster:~/submits/$2
+}
