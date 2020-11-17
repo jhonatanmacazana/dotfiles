d94f86ca6cbd55979256d3a77194353a513e3daf
Jhonatan Macazana <46355455+jhonatanmacazana@users.noreply.github.com>
2020-11-16 21:43:39 -0500

    fixed tmux-ssh, now working on right side status bar

---
+++ .config/tmux/ssh-utils.sh
@@ -0,0 +1,138 @@
+#/bin/bash
+
+# exit the script if any statement returns a non-true return value
+set -e
+
+_is_enabled() {
+    [ x"$1" = x"true" ] || [ x"$1" = x"yes" ] || [ x"$1" = x"enabled" ] || [ x"$1" = x"1" ]
+}
+
+_tty_info() {
+    tty="${1##/dev/}"
+    case "$_uname_s" in
+    *CYGWIN*)
+        ps -al | tail -n +2 | awk -v tty="$tty" '
+        ((/ssh/ && !/-W/) || !/ssh/) && $5 == tty {
+          user[$1] = $6; parent[$1] = $2; child[$2] = $1
+        }
+        END {
+          for (i in parent)
+          {
+            j = i
+            while (parent[j])
+              j = parent[j]
+
+            if (!(i in child) && j != 1)
+            {
+              file = "/proc/" i "/cmdline"; getline command < file; close(file)
+              gsub(/\0/, " ", command)
+              "id -un " user[i] | getline username
+              print i":"username":"command
+              exit
+            }
+          }
+        }
+      '
+        ;;
+    *)
+        ps -t "$tty" -o user=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX -o pid= -o ppid= -o command= | awk '
+        NR > 1 && ((/ssh/ && !/-W/) || !/ssh/) {
+          user[$2] = $1; parent[$2] = $3; child[$3] = $2; pid=$2; $1 = $2 = $3 = ""; command[pid] = substr($0,4)
+        }
+        END {
+          for (i in parent)
+          {
+            j = i
+            while (parent[j])
+              j = parent[j]
+
+            if (!(i in child) && j != 1)
+            {
+              print i":"user[i]":"command[i]
+              exit
+            }
+          }
+        }
+      '
+        ;;
+    esac
+}
+
+_ssh_or_mosh_args() {
+    case "$1" in
+    *ssh*)
+        args=$(printf '%s' "$1" | perl -n -e 'print if s/(.*?)\bssh\b\s+(.*)/\2/')
+        ;;
+    *mosh-client*)
+        args=$(printf '%s' "$1" | sed -E -e 's/.*mosh-client -# (.*)\|.*$/\1/' -e 's/-[^ ]*//g' -e 's/\d:\d//g')
+        ;;
+    esac
+
+    printf '%s' "$args"
+}
+
+_username() {
+    tty=${1:-$(tmux display -p '#{pane_tty}')}
+    ssh_only=$2
+
+    tty_info=$(_tty_info "$tty")
+    command=${tty_info#*:}
+    command=${command#*:}
+
+    ssh_or_mosh_args=$(_ssh_or_mosh_args "$command")
+    if [ -n "$ssh_or_mosh_args" ]; then
+        # shellcheck disable=SC2086
+        username=$(ssh -G $ssh_or_mosh_args 2>/dev/null | awk '/^user / { print $2; exit }')
+        # shellcheck disable=SC2086
+        [ -z "$username" ] && username=$(ssh -T -o ControlPath=none -o ProxyCommand="sh -c 'echo %%username%% %r >&2'" $ssh_or_mosh_args 2>&1 | awk '/^%username% / { print $2; exit }')
+    else
+        if ! _is_enabled "$ssh_only"; then
+            username=${tty_info#*:}
+            username=${username%%:*}
+        fi
+    fi
+
+    printf '%s\n' "$username"
+}
+
+_hostname() {
+    tty=${1:-$(tmux display -p '#{pane_tty}')}
+    ssh_only=$2
+    tty_info=$(_tty_info "$tty")
+    command=${tty_info#*:}
+    command=${command#*:}
+
+    ssh_or_mosh_args=$(_ssh_or_mosh_args "$command")
+    if [ -n "$ssh_or_mosh_args" ]; then
+        # shellcheck disable=SC2086
+        hostname=$(ssh -G $ssh_or_mosh_args 2>/dev/null | awk '/^hostname / { print $2; exit }')
+        # shellcheck disable=SC2086
+        [ -z "$hostname" ] && hostname=$(ssh -T -o ControlPath=none -o ProxyCommand="sh -c 'echo %%hostname%% %h >&2'" $ssh_or_mosh_args 2>&1 | awk '/^%hostname% / { print $2; exit }')
+
+        case "$hostname" in
+        *[a-z-].*)
+            hostname=${hostname%%.*}
+            ;;
+        127.0.0.1)
+            hostname="localhost"
+            ;;
+        esac
+    else
+        if ! _is_enabled "$ssh_only"; then
+            hostname=$3
+        fi
+    fi
+
+    printf '%s\n' "$hostname"
+}
+
+_root() {
+    tty=${1:-$(tmux display -p '#{pane_tty}')}
+    root=${2:-'!'}
+
+    username=$(_username "$tty" false)
+
+    [ x"$username" = x"root" ] && echo "$root"
+}
+
+"$@"
