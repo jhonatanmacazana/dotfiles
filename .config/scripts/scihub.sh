28b56680e8f165f957ee95409f11ef4926deae67
Jhonatan Macazana <46355455+jhonatanmacazana@users.noreply.github.com>
2020-11-21 22:08:43 -0500

    updated scihub script with current extension

--- .config/scripts/scihub.sh
+++ .config/scripts/scihub.sh
@@ -4,9 +4,9 @@
 # Get a single pdf with: ./sciHub.sh 10.1145/1375761.1375762
 # USe as many DOIs as arguents as you'd like :)
 # to pass a list of DOI strings as arguments to this script you could use: "cat DOIS.txt | xargs ./sciHub.sh"
-# replace .tw in the sci-hub url with whatever tld is currently in operation....
+# replace .se in the sci-hub url with whatever tld is currently in operation....
 
 for DOI in "$@"; do
-    wget https://sci-hub.tw/$DOI -qO - | grep -Eom1 'https://[^ ]+\.pdf' | wget -i -
+    wget https://sci-hub.se/$DOI -qO - | grep -Eom1 'https://[^ ]+\.pdf' | wget -i -
 done
 unset DOI
