3f7868b1fa30d893bbacad5ededcad8906b98f34
Jhonatan Macazana <46355455+jhonatanmacazana@users.noreply.github.com>
2021-01-01 18:44:23 -0500

    feat: adding ls_colors support

--- .config/shell/exports.sh
+++ .config/shell/exports.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# XDG variables 
+export XDG_CACHE_HOME=${XDG_CACHE_HOME:="$HOME/.cache"}
+export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:="$HOME/.config"}
+export XDG_DATA_HOME=${XDG_DATA_HOME:="$HOME/.local/share"}
+
 # Make vim the default editor.
 export EDITOR='vim'
 
